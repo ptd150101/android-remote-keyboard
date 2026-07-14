40619d3e06555ff07204e1cebd75596457fa3d7c
ptd150101 <101110862+ptd150101@users.noreply.github.com>
2026-07-14 18:01:02 +0700

    Add Android preference keys

---
+++ android-app/app/src/main/java/com/datpt/remotekey/Prefs.kt
@@ -0,0 +1,14 @@
+package com.datpt.remotekey
+
+object Prefs {
+    const val FILE = "remote_key_settings"
+    const val HOST = "host"
+    const val PORT = "port"
+    const val TOKEN = "token"
+    const val CAPTURE_ENABLED = "capture_enabled"
+    const val CONNECTION_STATUS = "connection_status"
+    const val LAST_KEY = "last_key"
+
+    const val DEFAULT_PORT = 45892
+    const val DEFAULT_TOKEN = "remotekey-123456"
+}
